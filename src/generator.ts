56c949229ce675aa8e8d31d93bd87ecdb2d83615
Taiyu Yoshizawa <taiyu31@gmail.com>
2021-11-23 00:46:03 +0900

    add: prettier & eslint

--- src/generator.ts
+++ src/generator.ts
@@ -1,35 +1,40 @@
-import * as sharp from "sharp";
-import {join, resolve} from "path";
-import {writeFileSync} from "fs";
-import type {ResizedImg} from "./index";
-import * as Buffer from "buffer";
+import * as sharp from 'sharp';
+import { join, resolve } from 'path';
+import { writeFileSync } from 'fs';
+import type { ResizedImg } from './index';
+import * as Buffer from 'buffer';
 
 type SpriteJsonType = {
   [key: string]: {
-    width: number,
-    height: number,
-    x: number,
-    y: number,
-    pixelRatio: 1 | 2,
-  }
-}
+    width: number;
+    height: number;
+    x: number;
+    y: number;
+    pixelRatio: 1 | 2;
+  };
+};
 
 type RectType = {
-  input: Buffer,
-  rect: { x: number, y: number, w: number, h:number },
-  id: string
-}
+  input: Buffer;
+  rect: { x: number; y: number; w: number; h: number };
+  id: string;
+};
 
-function copyImage(rect: RectType, buf: Buffer, bufDims:{w: number, h: number}) {
+function copyImage(rect: RectType, buf: Buffer, bufDims: { w: number; h: number }) {
   const r = { ...rect.rect };
 
-  function pxi(a: [x: number, y: number], dims: {w: number, h: number}) {
+  function pxi(a: [x: number, y: number], dims: { w: number; h: number }) {
     const [x, y] = a;
-    return y * (dims.w * 4) + (x * 4);
+    return y * (dims.w * 4) + x * 4;
   }
 
-  function copyPx(toBuffer: Buffer, toP: [x: number, y: number], fromBuffer: Buffer, fromP: [x: number, y: number]) {
-    let fromI = pxi(fromP, r);
+  function copyPx(
+    toBuffer: Buffer,
+    toP: [x: number, y: number],
+    fromBuffer: Buffer,
+    fromP: [x: number, y: number]
+  ) {
+    const fromI = pxi(fromP, r);
 
     if (fromI < fromBuffer.length) {
       toBuffer.writeUInt32LE(fromBuffer.readUInt32LE(fromI), pxi(toP, bufDims));
@@ -43,73 +48,88 @@ function copyImage(rect: RectType, buf: Buffer, bufDims:{w: number, h: number}) 
   }
 }
 // CompositeでCopyするとStack Overflowするためピクセルバッファでコピー
-function copyImages(bin: {w: number, h: number},images: RectType[], buf: Buffer) {
-
+function copyImages(bin: { w: number; h: number }, images: RectType[], buf: Buffer) {
   for (let i = 0; i < images.length; ++i) {
     copyImage(images[i], buf, bin);
   }
 }
 
-
-async function createFiles(imagePadding: number, normalImages: ResizedImg[], outputPath: string, outputFileName:string, is2x: boolean) {
-
+async function createFiles(
+  imagePadding: number,
+  normalImages: ResizedImg[],
+  outputPath: string,
+  outputFileName: string,
+  is2x: boolean
+) {
   const sortedImages: ResizedImg[] = normalImages.sort((a, b) => {
-    return b.height - a.height
-  })
-  let spriteJson: SpriteJsonType = {}
+    return b.height - a.height;
+  });
+  const spriteJson: SpriteJsonType = {};
   let x = imagePadding;
   let y = imagePadding;
-  let initWidth = Math.ceil(Math.sqrt(sortedImages.reduce((a,b)=> a + (b.width+imagePadding) * (b.height + imagePadding), 0)));
-  const maxWidth = Math.ceil(sortedImages.reduce((a,b)=> a.width>b.width ? a : b).width) + imagePadding
+  let initWidth = Math.ceil(
+    Math.sqrt(
+      sortedImages.reduce((a, b) => a + (b.width + imagePadding) * (b.height + imagePadding), 0)
+    )
+  );
+  const maxWidth =
+    Math.ceil(sortedImages.reduce((a, b) => (a.width > b.width ? a : b)).width) + imagePadding;
   if (initWidth < maxWidth) {
-    initWidth = maxWidth
+    initWidth = maxWidth;
   }
-  let height = 0
-  let nowYList: number[] = []
+  let height = 0;
+  let nowYList: number[] = [];
   const compositeImages: RectType[] = sortedImages.map((value, i) => {
     if (x + value.width + imagePadding > initWidth) {
-      y += nowYList.reduce((a,b)=>a>b?a:b) + imagePadding
-      x = imagePadding
-      height = y
-      nowYList = []
+      y += nowYList.reduce((a, b) => (a > b ? a : b)) + imagePadding;
+      x = imagePadding;
+      height = y;
+      nowYList = [];
     }
     spriteJson[value.id] = {
       width: value.width,
       height: value.height,
       x,
       y,
-      pixelRatio: is2x ? 2: 1
-    }
+      pixelRatio: is2x ? 2 : 1,
+    };
     const response: RectType = {
       input: value.img,
       rect: { x: x, y: y, w: value.width, h: value.height },
-      id: value.id
-    }
-    x += value.width + imagePadding
-    nowYList.push(value.height)
+      id: value.id,
+    };
+    x += value.width + imagePadding;
+    nowYList.push(value.height);
     if (sortedImages.length === i + 1) {
-      height += nowYList.reduce((a,b)=>a>b?a:b) + imagePadding
+      height += nowYList.reduce((a, b) => (a > b ? a : b)) + imagePadding;
     }
-    return response
-  })
+    return response;
+  });
   const file = await sharp({
     create: {
       width: height < initWidth ? initWidth : height,
       height: height < initWidth ? initWidth : height,
       channels: 4,
-      background: { r: 0, g: 0, b: 0, alpha: 0 }
-    }
-  }).raw().toBuffer()
-  const bin = {w:height < initWidth ? initWidth : height , h: height < initWidth ? initWidth : height}
-  copyImages(bin, compositeImages, file)
+      background: { r: 0, g: 0, b: 0, alpha: 0 },
+    },
+  })
+    .raw()
+    .toBuffer();
+  const bin = {
+    w: height < initWidth ? initWidth : height,
+    h: height < initWidth ? initWidth : height,
+  };
+  copyImages(bin, compositeImages, file);
   await sharp(file, {
     raw: {
       width: bin.w,
       height: bin.h,
       channels: 4,
-    }
-  }).png().toFile(resolve(join(outputPath, `${outputFileName}.png`)))
+    },
+  })
+    .png()
+    .toFile(resolve(join(outputPath, `${outputFileName}.png`)));
   writeFileSync(resolve(join(outputPath, `${outputFileName}.json`)), JSON.stringify(spriteJson));
 }
 
-export {createFiles}
+export { createFiles };
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,27 @@
-import {basename, isAbsolute, join, resolve} from 'path';
-import {sync} from 'glob'
-import {readFileSync} from "fs";
+import { basename, isAbsolute, join, resolve } from 'path';
+import { sync } from 'glob';
+import { readFileSync } from 'fs';
 import * as sharp from 'sharp';
-import {createFiles} from "./generator.js";
-
+import { createFiles } from './generator.js';
 
 type Img = {
-  img: Buffer,
-  id: string
-}
+  img: Buffer;
+  id: string;
+};
 
 export type ResizedImg = {
-  img: Buffer,
-  id: string,
-  width: number,
-  height: number
-}
-
+  img: Buffer;
+  id: string;
+  width: number;
+  height: number;
+};
 
 type GenerateOptions = {
   padding: number;
   inputPath: string;
   outputPath: string;
   outputFileName: string;
-}
+};
 
 /**
  *
@@ -33,58 +31,63 @@ type GenerateOptions = {
  * @param outputFileName {string} Name of the generated sprite. The output will look like [outputFileName].png,[outputFileName].json,[outputFileName]@2x.png,[outputFileName]@2x.json. example: sprite
  */
 async function generate({
-                          padding = 0,
-                          inputPath,
-                          outputPath,
-                          outputFileName = "sprite"
-                        }: GenerateOptions) {
-  const imagePadding = 10 + padding
+  padding = 0,
+  inputPath,
+  outputPath,
+  outputFileName = 'sprite',
+}: GenerateOptions) {
+  const imagePadding = 10 + padding;
   if (!inputPath) {
-    throw new Error("inputPath is required")
+    throw new Error('inputPath is required');
   }
   if (!outputPath) {
-    throw new Error("outputPath is required")
+    throw new Error('outputPath is required');
   }
   if (!isAbsolute(inputPath)) {
-    throw new Error("inputPath must be an absolute path")
+    throw new Error('inputPath must be an absolute path');
   }
   if (!isAbsolute(outputPath)) {
-    throw new Error("outputPath must be an absolute path")
+    throw new Error('outputPath must be an absolute path');
   }
-  const inputFullPath = resolve(join(inputPath, '*'))
+  const inputFullPath = resolve(join(inputPath, '*'));
   const input: Img[] = sync(inputFullPath).map((f) => {
     return {
       img: readFileSync(f),
-      id: basename(f).split('.').shift() ?? basename(f)
+      id: basename(f).split('.').shift() ?? basename(f),
     };
-  })
+  });
   if (!input.length) {
-    throw new Error("The input folder must contain at least one image file.")
+    throw new Error('The input folder must contain at least one image file.');
   }
 
-  const retinaImages: ResizedImg[] = []
-  const normalImagesPromise = input.map(({img, id}) => {
+  const retinaImages: ResizedImg[] = [];
+  const normalImagesPromise = input.map(({ img, id }) => {
     return (async (img, id) => {
-      const s1x = await sharp(img).ensureAlpha().png().raw().toBuffer({resolveWithObject: true})
-      const s2x = await sharp(img).resize(s1x.info.width * 2).ensureAlpha().png().raw().toBuffer({resolveWithObject: true})
+      const s1x = await sharp(img).ensureAlpha().png().raw().toBuffer({ resolveWithObject: true });
+      const s2x = await sharp(img)
+        .resize(s1x.info.width * 2)
+        .ensureAlpha()
+        .png()
+        .raw()
+        .toBuffer({ resolveWithObject: true });
       retinaImages.push({
         img: s2x.data,
         id: id,
         width: s2x.info.width,
-        height: s2x.info.height
-      })
+        height: s2x.info.height,
+      });
       return {
         img: s1x.data,
         id: id,
         width: s1x.info.width,
-        height: s1x.info.height
-      }
-    })(img, id)
-  })
-  const normalImages = await Promise.all(normalImagesPromise)
-  await createFiles(imagePadding, normalImages, outputPath, outputFileName, false)
-  await createFiles(imagePadding, retinaImages, outputPath, outputFileName + "@2x", true)
+        height: s1x.info.height,
+      };
+    })(img, id);
+  });
+  const normalImages = await Promise.all(normalImagesPromise);
+  await createFiles(imagePadding, normalImages, outputPath, outputFileName, false);
+  await createFiles(imagePadding, retinaImages, outputPath, outputFileName + '@2x', true);
 }
 
-export {generate};
+export { generate };
 export default generate;
